62674c0056efb2017c711f4f87a11889e3ce7e66
Michael Bloem <mbloem@steelcase.com>
2018-06-12 09:26:18 -0400

    Changed version to 0.4.1-Steelcase so that we can distinguish our branch

--- traces/__init__.py
+++ traces/__init__.py
@@ -7,6 +7,6 @@ from traces import decorators
 from traces import operations
 from infinity import inf
 
-__author__ = 'Mike Stringer'
-__email__ = 'mike.stringer@datascopeanalytics.com'
-__version__ = '0.4.1'
+__author__ = 'Steelcase, Inc.'
+__email__ = 'zdresche@steelcase.com'
+__version__ = '0.4.1-Steelcase'
